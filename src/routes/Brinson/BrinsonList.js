c6ecb3a91c35cf9b070c670b7450216d5ef59bbe
A-lin <952569081@qq.com>
2018-06-21 14:52:46 +0800

    Update BrinsonList.js

--- src/routes/Brinson/BrinsonList.js
+++ src/routes/Brinson/BrinsonList.js
@@ -14,29 +14,20 @@ import {
   Dropdown,
 } from 'antd';
 import numeral from 'numeral';
-import {
-  ChartCard,
-  yuan,
-  MiniArea,
-  MiniBar,
-  MiniProgress,
-  Field,
-  Bar,
-  Pie,
-  TimelineChart,
-} from 'components/Charts';
+import { Bar } from 'components/Charts';
 import Trend from 'components/Trend';
 import NumberInfo from 'components/NumberInfo';
 import { getTimeDistance } from '../../utils/utils';
 import { NavigationBar } from './NavigationBar';
 import styles from './BrinsonList.less';
-//var echarts = require('echarts');
 
 const { TabPane } = Tabs;
 const { RangePicker } = DatePicker;
 
 const Yuan = ({ children }) => (
-  <span dangerouslySetInnerHTML={{ __html: yuan(children) }} /> /* eslint-disable-line react/no-danger */
+  <span
+    dangerouslySetInnerHTML={{ __html: yuan(children) }}
+  /> /* eslint-disable-line react/no-danger */
 );
 
 @connect(({ chart, loading }) => ({
@@ -65,28 +56,23 @@ export default class BrinsonList extends Component {
 
   render() {
     const { chart, loading } = this.props;
-    const {
-      indexData,
-      exContribution,
-      configData,
-      stockcrossData,
-    } = chart;
+    const { indexData, exContribution, configData, stockcrossData } = chart;
 
-    const brinsonData = [];//数据 brinson数据
-    for(let i=0;i<indexData.length;i++){
+    const brinsonData = []; //数据 brinson数据
+    for (let i = 0; i < indexData.length; i++) {
       brinsonData.push({
-          index:(i+1),
-          x:indexData[i],
-          y:exContribution[i]
+        index: i + 1,
+        x: indexData[i],
+        y: exContribution[i],
       });
     }
 
-    const brinsonData2=[]; //行业配置和交叉股
-    for(let i=0;i<indexData.length;i++){
+    const brinsonData2 = []; //行业配置和交叉股
+    for (let i = 0; i < indexData.length; i++) {
       brinsonData2.push({
-          index:(i+1),
-          x:indexData[i],
-          y:[configData[i],stockcrossData[i]]
+        index: i + 1,
+        x: indexData[i],
+        y: [configData[i], stockcrossData[i]],
       });
     }
 
@@ -103,37 +89,31 @@ export default class BrinsonList extends Component {
         //sorter: (a, b) => a.count - b.count,
         className: styles.alignRight,
       },
-      
     ];
 
     return (
       <Fragment>
-         
-        <NavigationBar currentKey={this.state.currentTabKey}/>
+        <NavigationBar currentKey={this.state.currentTabKey} />
         <Card loading={loading} bordered={false}>
-        <div className={styles.salesCard}>
-          <div className={styles.salesBar}>
-            <div id="configurationBar"></div>
-            <Bar height={295} title="超额贡献" data={brinsonData} />
+          <div className={styles.salesCard}>
+            <div className={styles.salesBar}>
+              <div id="configurationBar" />
+              <Bar height={295} title="超额贡献" data={brinsonData} />
+            </div>
           </div>
-           </div>
         </Card>
-          <Card
-            loading={loading}
-            bordered={false}
-            style={{ marginTop: 24 }} >
-            <Table
-              rowKey={record => record.index}
-              size="small"
-              columns={columns}
-              dataSource={brinsonData}
-              pagination={{
-                style: { marginBottom: 0 },
-                pageSize:100,
-              }}
-            />
-          </Card>
-         
+        <Card loading={loading} bordered={false} style={{ marginTop: 24 }}>
+          <Table
+            rowKey={record => record.index}
+            size="small"
+            columns={columns}
+            dataSource={brinsonData}
+            pagination={{
+              style: { marginBottom: 0 },
+              pageSize: 100,
+            }}
+          />
+        </Card>
       </Fragment>
     );
   }
