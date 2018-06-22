5de95ad259a7e6fc8d60279cf99613fd99018e2e
A-lin <952569081@qq.com>
2018-06-22 10:00:57 +0800

    Add files via upload

---
+++ src/routes/FactorCheck.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'dva';
+import { Row, Col, Card, Tabs, Table, Button, Icon } from 'antd';
+import styles from './FactorCheck.less';
+
+var $ = require('jquery');
+var common = require('../../utils/common');
+
+export default class FactorCheck extends Component {
+  render() {
+    return <div>FactorCheck</div>;
+  }
+}
---
+++ src/routes/FactorMessage.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'dva';
+import { Row, Col, Card, Tabs, Table, Button, Icon } from 'antd';
+import styles from './FactorMessage.less';
+
+var $ = require('jquery');
+var common = require('../../utils/common');
+
+export default class FactorMessage extends Component {
+  render() {
+    return <div>FactorMessage</div>;
+  }
+}
